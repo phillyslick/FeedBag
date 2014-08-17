9f6d42432b73caa0f352d4bec1bf0f1423f89ca3
Philly Slick <noesis@philadelphiaslick.com>
2014-08-17 02:02:03 -0400

    Dummy app testing ready to roll

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,9 @@
-ENV["RAILS_ENV"] = "test"
-
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
 require 'FeedBag'
 require "FeedBag/FeedGrabber"
 require 'vcr'
-Rails.backtrace_cleaner.remove_silencers!
+
 # Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/factories/vcr_cassettes'
